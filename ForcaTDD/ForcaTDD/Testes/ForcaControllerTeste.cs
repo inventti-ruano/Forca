7e1d0fd8561ee98bbe97babf7c29fb266a72c280
Ruano Martinez <ruano@inventti.com.br>
2014-01-15 14:46:04 -0200

    Forca

---
+++ ForcaTDD/ForcaTDD/Testes/ForcaControllerTeste.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace ForcaTDD.Testes
+{
+    class ForcaControllerTeste
+    {
+        [Test]
+        public void InstanciaJogoDaForca()
+        {
+            var forca = new ForcaController();
+        }
+    }
+}
